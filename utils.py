277b4eedd8eea5c8b0c3acc4c03a7bcf26354dc3
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2024-12-13 02:13:31 +0900

    initial commit

---
+++ utils.py
@@ -0,0 +1,261 @@
+import random, string
+
+def wcst_generator(rule, randomize=False):
+    if rule not in ["color", "shape", "number"]:
+        raise Exception("Rule not recognized")
+    
+    choices = {"number": ["one", "two", "three", "four"],
+               "color": ["red", "yellow", "blue", "green"],
+               "shape": ["circle", "triangle", "star", "cross"],
+               }
+
+    given_card = {cat: random.choice(choices[cat]) for cat in choices.keys()}
+    
+    card_set = []
+
+    chosen = {}
+    while chosen == given_card or chosen == {}:
+        for cat in choices.keys():
+            if cat != rule:
+                chosen[cat] = random.choice(choices[cat])
+            else:
+                chosen[cat] = given_card[cat]
+    card_set.append(chosen)
+
+    for _ in range(3):
+        lure = {}
+        while (lure == given_card or
+               lure in card_set or
+               lure == {}):
+            for cat in choices.keys():
+                if cat != rule:
+                    lure[cat] = random.choice(choices[cat])
+                else:
+                    lure[cat] = random.choice([choice for choice in choices[cat] if choice != given_card[cat]])
+
+        card_set.append(lure)
+    
+    if randomize:
+        card_set = [random.sample(list(card.values()), k=len(card.values())) for card in card_set]
+    else:
+        card_set = [list(card.values()) for card in card_set]
+
+    card_set = [' '.join(card) for card in card_set]
+    given_card = ' '.join(list(given_card.values()))
+
+    return given_card, card_set
+
+def string_generator(rule, max_given_length=10):
+    letters = string.ascii_letters
+    vowels = "aeiouAEIOU"
+    consonants = ''.join([char for char in letters if char not in vowels])
+
+    # odd numbers only to avoid equal number of vowels and consonants
+    given_length = random.choice(range(3,  max_given_length, 2)) 
+    given_string = ''.join(random.choices(letters, k=given_length))
+
+    string_set = []
+    if rule == "length":
+        length = len(given_string)
+        
+        chosen = ''
+        while chosen == given_string or chosen == '':
+            chosen = ''.join(random.choices(letters, k=length))
+        string_set.append(chosen)
+
+        for _ in range(3):
+            len_range = list(range(1,  int(given_length*1.5)))
+            len_range.remove(length)
+            lure_length = random.choice(len_range)
+
+            lure = ''
+            while (lure == given_string or
+                  lure in string_set or
+                  lure == ''):
+                lure = ''.join(random.choices(letters, k=lure_length))
+            string_set.append(lure)
+            
+    elif rule == "vowels":
+        cnt = count_vowels(given_string)
+
+        # odd numbers only to avoid equal number of vowels and consonants
+        min_length = max(cnt, int(given_length*0.5))
+        if min_length % 2 == 0:
+            min_length += 1
+        length = random.choice(range(min_length, int(given_length*1.5), 2))
+        
+        chosen = ''
+        while chosen == given_string or chosen == '':
+
+            chosen = ''.join(random.choices(vowels, k=cnt))
+            chosen += ''.join(random.choices(consonants, k=length-cnt))
+            char_list = list(chosen)
+            random.shuffle(char_list)
+            chosen = ''.join(char_list)
+        string_set.append(chosen)
+
+        for _ in range(3):
+            length = random.choice(range(min_length, int(given_length*1.5), 2)) 
+            
+            cnt_range = list(range(0,  length+1))
+            if cnt in cnt_range:
+                cnt_range.remove(cnt)
+            lure_cnt = random.choice(cnt_range)
+            
+            lure = ''
+            while (lure == given_string or
+                  lure in string_set or
+                  lure == ''):
+                lure = ''.join(random.choices(vowels, k=lure_cnt))
+                lure += ''.join(random.choices(consonants, k=length-lure_cnt))
+                char_list = list(lure)
+                random.shuffle(char_list)
+                lure = ''.join(char_list)
+            string_set.append(lure)
+
+    elif rule == "consonants":
+        cnt = count_vowels(given_string)
+        
+        # odd numbers only to avoid equal number of vowels and consonants
+        min_length = max(given_length - cnt, int(given_length*0.5))
+        if min_length % 2 == 0:
+            min_length += 1
+        length = random.choice(range(min_length, int(given_length*1.5), 2))
+
+        cnt = len(given_string) - count_vowels(given_string)
+        
+        chosen = ''
+        while chosen == given_string or chosen == '':
+            chosen = ''.join(random.choices(consonants, k=cnt))
+            chosen += ''.join(random.choices(vowels, k=length-cnt))
+            char_list = list(chosen)
+            random.shuffle(char_list)
+            chosen = ''.join(char_list)
+        string_set.append(chosen)
+
+        for _ in range(3):
+            length = random.choice(range(min_length,  int(given_length*1.5), 2)) 
+            
+            cnt_range = list(range(0,  length+1))
+            if cnt in cnt_range:
+                cnt_range.remove(cnt)
+            lure_cnt = random.choice(cnt_range)
+            
+            lure = ''
+            while (lure == given_string or
+                  lure in string_set or
+                  lure == ''):                
+                lure = ''.join(random.choices(consonants, k=lure_cnt))
+                lure += ''.join(random.choices(vowels, k=length-lure_cnt))
+                char_list = list(lure)
+                random.shuffle(char_list)
+                lure = ''.join(char_list)
+            string_set.append(lure)
+    else:
+        raise Exception("Rule not recognized")
+
+    return given_string, string_set
+
+def count_vowels(str):
+    vowels = "aeiouAEIOU"
+    return sum(1 for char in str if char in vowels)
+
+def generate_few_shot(variant):
+    prompt = "Example of a short session:\n"
+    if variant == "card":
+        rules = ["number", "color", "shape"]
+
+        for rule in rules:
+            if rules.index(rule) != 0:
+                prompt += "Correct!\n"
+                
+            given_card, card_set = wcst_generator(rule)
+            true_ans = card_set[0]
+            ans = card_set[1] # Arbitrary wrong choice
+            random.shuffle(card_set)
+            ans = card_set.index(ans)
+            true_ans = card_set.index(true_ans)
+
+            prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+            prompt += f"ANSWER: {ans}\n\n"
+            prompt += "Incorrect. Please try again.\n"
+            prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+            prompt += f"ANSWER: {true_ans}\n\n"
+
+            for _ in range(3):
+                prompt += "Correct!\n"
+
+                given_card, card_set = wcst_generator(rule)
+                true_ans = card_set[0]
+                random.shuffle(card_set)
+                true_ans = card_set.index(true_ans)
+
+                prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+                prompt += f"ANSWER: {true_ans}\n\n"
+
+    elif variant == "card-random":
+        rules = ["number", "color", "shape"]
+
+        for rule in rules:
+            if rules.index(rule) != 0:
+                prompt += "Correct!\n"
+
+            given_card, card_set = wcst_generator(rule, randomize=True)
+            true_ans = card_set[0]
+            ans = card_set[1] # Arbitrary wrong choice
+            random.shuffle(card_set)
+            ans = card_set.index(ans)
+            true_ans = card_set.index(true_ans)
+
+            prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+            prompt += f"ANSWER: {ans}\n\n"
+            prompt += "Incorrect. Please try again.\n"
+            prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+            prompt += f"ANSWER: {true_ans}\n\n"
+
+            for _ in range(3):
+                prompt += "Correct!\n"
+
+                given_card, card_set = wcst_generator(rule, randomize=True)
+                true_ans = card_set[0]
+                random.shuffle(card_set)
+                true_ans = card_set.index(true_ans)
+
+                prompt += f"Given: {given_card}\nOptions:\n1. {card_set[0]}\n2. {card_set[1]}\n3. {card_set[2]}\n4. {card_set[3]}\n\n"
+                prompt += f"ANSWER: {true_ans}\n\n"
+
+    elif variant == "string":
+        rules = ["length", "vowels", "consonants"]
+
+        for rule in rules:
+            if rules.index(rule) != 0:
+                prompt += "Correct!\n"
+
+            given_string, string_set = string_generator(rule)
+            true_ans = string_set[0]
+            ans = string_set[1] # Arbitrary wrong choice
+            random.shuffle(string_set)
+            ans = string_set.index(ans)
+            true_ans = string_set.index(true_ans)
+
+            prompt += f"Given: {given_string}\nOptions:\n1. {string_set[0]}\n2. {string_set[1]}\n3. {string_set[2]}\n4. {string_set[3]}\n\n"
+            prompt += f"ANSWER: {ans}\n\n"
+            prompt += "Incorrect. Please try again.\n"
+            prompt += f"Given: {given_string}\nOptions:\n1. {string_set[0]}\n2. {string_set[1]}\n3. {string_set[2]}\n4. {string_set[3]}\n\n"
+            prompt += f"ANSWER: {true_ans}\n\n"
+
+            for _ in range(3):
+                prompt += "Correct!\n"
+
+                given_string, string_set = string_generator(rule)
+                true_ans = string_set[0]
+                random.shuffle(string_set)
+                true_ans = string_set.index(true_ans)
+
+                prompt += f"Given: {given_string}\nOptions:\n1. {string_set[0]}\n2. {string_set[1]}\n3. {string_set[2]}\n4. {string_set[3]}\n\n"
+                prompt += f"ANSWER: {true_ans}\n\n"
+
+    else:
+        raise Exception("Variant not recognized")
+    
+    return prompt
---
+++ wcst.py
@@ -0,0 +1,196 @@
+import transformers
+import google.generativeai as genai
+from tqdm.auto import tqdm
+
+import json, argparse, random, os
+
+from .utils import generate_few_shot, wcst_generator, string_generator
+
+wcst_prompt = """You are performing the Wisconsin Card Sorting Test (WCST).
+You will be shown a given card with a symbol on it, and you will have to match it to one of four option cards according to an attribute that you have to figure out.
+The cards will be described by the following attributes:
+1. Number of symbols
+2. Color of symbols
+3. Shape of symbols
+
+You will be told "Correct!" if you are correct and "Incorrect. Please try again." if you are incorrect.
+If you are incorrect, you have to consider the feedback and try a different attribute to match the cards.
+If you are correct, you have to stick with the same attribute until you are incorrect.
+Keep performing the task until the end of the test.
+Answer with the index of the card you think is the correct match.
+State your final answer using the template: "ANSWER: <index>"
+
+"""
+wcst_random_prompt = """You are performing the Wisconsin Card Sorting Test (WCST).
+You will be shown a given card with a symbol on it, and you will have to match it to one of four option cards according to an attribute that you have to figure out.
+The cards will be described by the following attributes in a random order:
+1. Number of symbols
+2. Color of symbols
+3. Shape of symbols
+
+You will be told "Correct!" if you are correct and "Incorrect. Please try again." if you are incorrect.
+If you are incorrect, you have to consider the feedback and try a different attribute to match the cards.
+If you are correct, you have to stick with the same attribute until you are incorrect.
+Keep performing the task until the end of the test.
+Answer with the index of the card you think is the correct match.
+State your final answer using the template: "ANSWER: <index>"
+
+"""
+random_prompt = """You are performing a modified version of the Wisconsin Card Sorting Test (WCST).
+You will be shown a given string, and you have to match it with one of four option strings according to a rule that you have to figure out.
+The rule is one of the following:
+1. Length of the string
+2. The number of vowels in the string
+3. The number of consonants in the string
+
+You will be told "Correct!" if you are correct and "Incorrect. Please try again." if you are incorrect.
+If you are incorrect, you have to consider the feedback and try a different rule to match the strings.
+If you are correct, you have to stick with the same rule until you are incorrect.
+Keep performing the task until the end of the test.
+Answer with the index of the string you think is the correct match.
+State your final answer using the template: "ANSWER: <index>"
+
+"""
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="llama")
+    parser.add_argument("--variant", type=str, default="wcst")
+    parser.add_argument("--max_trials", type=int, default=64)
+    parser.add_argument("--num_correct", type=int, default=5)
+    parser.add_argument("--few_shot", type=bool, default=False)
+    parser.add_argument("--cot", type=bool, default=False)
+    parser.add_argument("--verbose", type=int, default=15)
+    
+    args = parser.parse_args()
+    variant = args.variant
+    max_trials = args.max_trials
+    num_correct = args.num_correct
+    few_shot = args.few_shot
+    cot = args.cot
+
+    if variant == "card":
+        system_prompt = wcst_prompt
+        rules = ["color", "shape", "number"]
+    elif variant == "card-random":
+        system_prompt = wcst_random_prompt
+        rules = ["color", "shape", "number"]
+    elif variant == "string":
+        system_prompt = random_prompt
+        rules = ["length", "vowels", "consonants"]
+    else:
+        raise Exception("Variant not recognized")
+
+    if few_shot:
+        system_prompt += generate_few_shot(variant)
+
+    if args.model == "llama":
+        pipeline = transformers.pipeline(
+        "text-generation",
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        )
+
+        messages = [{"role": "system", "content": system_prompt},]
+
+    elif args.model == "gemini":
+        # api_key = os.getenv("GOOGLE_API_KEY")
+        # genai.configure(api_key=api_key)
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        chat = model.start_chat(
+            history=[
+                {"role": "user", "parts": "I will explain how you should behave.\n" + system_prompt},
+                {"role": "model", "parts": "Understood. Let's start the test.\n"},
+                ]
+        )
+    else:
+        raise Exception("Model not recognized")
+    
+    save_path = f"{args.model}_{variant}_{max_trials}-{num_correct}.json"
+    if few_shot:
+        save_path = save_path.replace(".json", "_few_shot.json")
+    if cot:
+        save_path = save_path.replace(".json", "_cot.json")
+
+    save = []
+    
+    n_trials = 0
+    completed_cat = 0
+    total_correct = 0
+    correct_prefix = ""
+
+    with tqdm(total=max_trials, desc="Total trials") as pbar:
+        for _ in range(2):      
+            for rule in rules:
+                correct_cnt = 0
+                
+                for trial in tqdm(range(num_correct), total=num_correct, desc=f"Correct responses for {rule}"):
+                    if variant == "card":
+                        given, opt = wcst_generator(rule, False)
+                    elif variant == "card-random":
+                        given, opt = wcst_generator(rule, True)
+                    else:
+                        given, opt = string_generator(rule)
+
+                    chosen = opt[0]
+                    random.shuffle(opt)
+                    chosen_idx = opt.index(chosen) + 1
+    
+                    test_prompt = f"""Given: {given}
+Options:
+1. {opt[0]}
+2. {opt[1]}
+3. {opt[2]}
+4. {opt[3]}
+"""
+    
+                    correct = False
+                    while not correct:
+                        if n_trials >= max_trials:
+                            break
+                        pbar.update(1)
+    
+                        n_trials += 1
+
+                        if args.model == "llama":
+                            messages.append({"role": "user", "content": correct_prefix + test_prompt})
+
+                            messages = pipeline(messages, 
+                                                max_new_tokens=500,
+                                                pad_token_id=pipeline.tokenizer.eos_token_id)[0]["generated_text"]
+                            response = messages[-1]["content"]
+                            ans = response.split("ANSWER: ")[-1].strip()
+
+                        elif args.model == "gemini":
+                            response = chat.send_message(correct_prefix + test_prompt).text
+                            ans = response.split("ANSWER: ")[-1].strip()
+
+                        if len(ans) != 1:
+                            correct_prefix = """Answer not found. Please state your answer using the template: \"ANSWER: <index>\""""
+                        elif ans == str(chosen_idx):
+                            correct_prefix = "Correct!\n"
+                            correct = True
+                            correct_cnt += 1
+                            total_correct += 1
+                        else:
+                            correct_prefix = "Incorrect. Please try again.\n"
+
+                        if n_trials % 15 == 0:
+                            tqdm.write(test_prompt)
+                            tqdm.write(response)
+
+                        save_row = {"rule": rule,
+                                    "correct": correct,
+                                    "question": test_prompt,
+                                    "response": response,
+                                    "model_ans": ans,
+                                    "true_ans": chosen_idx}
+                        save.append(save_row)
+                        with open(save_path, "w") as f:
+                            json.dump(save, f, indent=4)
+                
+                if correct_cnt == num_correct:
+                    completed_cat += 1
+
+    print(f"Completed categories: {completed_cat}")
+    print(f"Total number of trials: {n_trials}")
+    print(f"Total accuracy: {total_correct/n_trials}")
