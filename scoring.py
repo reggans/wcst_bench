e4e756d54261c8cc0672cb192b43303de16c83e2
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2024-12-16 18:19:46 +0900

    update score

--- scoring.py
+++ scoring.py
@@ -9,14 +9,22 @@ def score(file_path):
     for trial in data:
         current_rule = ""
         completion_lengths = []
+        num_correct = 0
 
         for query in trial:
             if query['rule'] != current_rule:
                 current_rule = query['rule']
                 completion_lengths.append(0)
+                num_correct = 0
             else:
                 completion_lengths[-1] += 1
+            
+            if query['correct']:
+                num_correct += 1
         
-        score = (len(completion_lengths) - 1) / len(completion_lengths) * np.sum(completion_lengths)
+        # if num_correct < 5:
+        #     completion_lengths
+        # score = (len(completion_lengths) - 1) / len(completion_lengths) * np.sum(completion_lengths)
+        score = np.sum([1/l for l in completion_lengths])
         scores.append(score)
     return np.mean(scores)
